1e4f6052ce401206620100adbf7020e70ba958f1
EunJinCHO <till5941@gmail.com>
2025-01-09 14:44:00 +0900

    동적 레이아웃, 텍스트 줄바뀜 제한, 우선순위
    동적 레이아웃, 텍스트 줄바뀜 제한, 우선순위 실습

--- SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -9,57 +9,38 @@ import SwiftUI
 
 // Main Content View
 struct ContentView: View {
+    @State var myLayout: AnyLayout = AnyLayout(VStackLayout())
+    
     var body: some View {
-        VStack{
-            HStack {
-                Image(systemName: "goforward.10")
-                Image(systemName: "goforward.15")
-                Image(systemName: "goforward.30")
+        myLayout{
+            Text("Sample Code")
+            Text("Sample Code")
+            Text("Sample Code")
+        }
+        HStack {
+            Button(action: {
+                myLayout = AnyLayout(HStackLayout())
+            }) {
+                Text("HStack")
             }
-            ZStack {
-                Image(systemName: "goforward.10")
-                Image(systemName: "goforward.15")
-                Image(systemName: "goforward.30")
+            Button(action: {
+                myLayout = AnyLayout(VStackLayout())
+            }) {
+                Text("VStack")
             }
-            // 복잡한 중첩 레이아웃 예시
-            Text("Financial Results")
-                .font(.title)
-            HStack {
-                Text("Q1 Sales")
-                    .font(.headline)
-                VStack {
-                    Text("January")
-                    Text("February")
-                    Text("March")
-                }
-                VStack {
-                    Text("$1000")
-                    Text("$200")
-                    Text("$3000")
-                }
-            }
-            // 정렬, 여백
-            HStack(alignment: .top) {
-                Text("Q1 Sales")
-                    .font(.headline)
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text("January")
-                    Text("February")
-                    Text("March")
-                }
-                Spacer()
-                VStack(alignment: .leading) {
-                    Text("$1000")
-                    Text("$2000")
-                    Text("$3000")
-                }
-            }
-            
         }
         
+        HStack {
+            Image(systemName: "airplane")
+            Text("Flight times: ")
+                .layoutPriority(1)      // 우선순위 부여
+            Text("London")
+        }
+        .font(.largeTitle)
+        .lineLimit(1)
     }
 }
+
 
 #Preview {
     ContentView()
