b31702c2cc758afdf1133095527cee364001db20
EunJinCHO <till5941@gmail.com>
2025-01-10 10:31:05 +0900

    async await
    비동기 실습

--- SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -9,20 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var showAlert = false
-    
     var body: some View {
-        Button("Show Alert") {
-            showAlert = true
+        Button(action: {
+            Task{
+                await doSomething()
+            }
+        }) {
+            Text("Do Something")
         }
-        // 경고창 팝업을 띄우는 수정자
-        .alert("Important Message",
-               isPresented: $showAlert,
-               actions: {
-               Button("OK", role: .cancel) { }
-        }, message: {
-            Text("This is an alert message.")
-        })
+    }
+    func doSomething() async {
+        print("Start \(Date())")
+        // 비동기 반환값을 위한 async let
+        async let result = takesTooLong()
+        print("After async-let \(Date())")
+        // async let 상수를 사용하려면, await
+        print("End \(await result)")
+        print("End \(Date())")
+    }
+    func takesTooLong() async -> Date {
+        sleep(5)
+        return Date()
     }
 }
 
