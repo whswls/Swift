36428ee32b78d0173d93d26a743433ea3d307039
EunJinCHO <till5941@gmail.com>
2025-01-10 09:38:56 +0900

    Tap 실습
    Tap 카운트 실습

--- SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -9,46 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var colors: [Color] = [.black , .red , .green , .blue]
-    var colornames = ["Black", "Red", "Green", "Blue"]
-    
-    @State private var colorIndex = 0
-    @State private var rotation: Double = 0
-    @State private var text: String = "Welcome to SwiftUI"
+    @State private var isTapped = false
     
     var body: some View {
-        VStack {
-            VStack {
-                Spacer()        // 가변적 공간
-                Text("Hello, world!")
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .rotationEffect(.degrees(rotation))
-                // 5초 동안 애니메이션 실행
-                    .animation(.easeInOut(duration: 5), value: rotation)
-                    .foregroundStyle(colors[colorIndex])
-                Spacer()
-                Divider()
-                Slider(value: $rotation, in: 0 ... 360, step: 0.1)
-                    .padding()
-                TextField("Enter text here", text: $text)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                Picker(selection: $colorIndex, label: Text("Color")) {
-                    ForEach (0 ..< colornames.count, id:\.self) { color in
-                        Text(colornames[color])
-                            .foregroundColor(colors[color])
-                    }
-                }
-                .pickerStyle(.wheel)
-                .padding()
+        Text("Tap me!")
+            .font(.largeTitle)
+            .padding()
+            .background(isTapped ? Color.green: Color.red)
+            .animation(.default, value: isTapped)
+            // Tap 카운트를 이용해 실행시킬 탭 횟수를 조절할 수 있음
+            .onTapGesture(count: 3) {
+                isTapped.toggle()
             }
-        }
     }
 }
-let name = { () -> Void in
-    print("Hello!")
-}
+
 
 
 #Preview {
