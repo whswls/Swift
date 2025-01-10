ff304c1e780e408f68d0cad70c5fdcd786904f9f
EunJinCHO <till5941@gmail.com>
2025-01-10 11:23:53 +0900

    작업 그룹
    작업 그룹 실습

--- SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -18,6 +18,11 @@ struct ContentView: View {
         Button(action: {
             // 작업의 우선순위를 결정할 수 있음
             Task(priority: .high) {
+                // 작업의 우선순위를 확인하는 프로퍼티
+                _ = Task.currentPriority
+                // 작업의 취소 여부를 확인하는 프로퍼티
+                _ = Task.isCancelled
+                await Task.yield()
                 await doSomething()
             }
             // 분리된 작업
@@ -32,30 +37,21 @@ struct ContentView: View {
         }
     }
     func doSomething() async {
-        print("Start \(Date())")
-        do {
-            try await takesTooLong(delay: 25)
-        } catch DurationError.tooShort {
-            print("Error: Duration too short")
-        } catch DurationError.tooLong {
-            print("Error: Duration too long")
-        } catch {
-            print("Unknown error")
+        await withTaskGroup(of: Void.self) { group in
+            for i in 1...5 {
+                group.addTask {
+                    let result = await takesTooLong()
+                    print("Completed Task \(i) = \(result)")
+                }
+            }
         }
-        print("End \(Date())")
     }
     
-    func takesTooLong(delay: UInt32) async throws {
-        if delay < 5 {
-            throw DurationError.tooShort
-        } else if delay > 20 {
-            throw DurationError.tooLong
-        }
-        sleep(delay)
-        print("Async task completed at \(Date())")
+    func takesTooLong() async -> Date {
+        sleep(5)
+        return Date()
     }
 }
-
 
 
 #Preview {
