84f211a71c9b4f27ce8e7cf39b297668341b6603
EunJinCHO <till5941@gmail.com>
2025-01-13 11:28:49 +0900

    iOS 18 버전 이후 탭
    iOS 18 버전 이후 탭

--- SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -11,18 +11,14 @@ struct ContentView: View {
     
     var body: some View {
         TabView {
-            FirstTabView()
-                .tabItem {
-                    Image(systemName: "01.circle")
-                    Text("First")
-                }
-            SecondTabView()
-                .tabItem {
-                    Image(systemName: "02.circle")
-                    Text("Second")
-                }
+            Tab("First", systemImage: "01.circle") {
+                FirstTabView()
+            }
+            Tab("Second", systemImage: "02.circle") {
+                SecondTabView()
+            }
         }
-        
+        .ignoresSafeArea(.all)
     }
 }
 
