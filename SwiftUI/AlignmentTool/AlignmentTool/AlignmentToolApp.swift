75c875e6da0aba6b7c8c2020836ffc7296a2cb3c
EunJinCHO <till5941@gmail.com>
2025-01-14 10:37:09 +0900

    AlignmentTool
    AlignmentTool 코드 추가

---
+++ SwiftUI/AlignmentTool/AlignmentTool/AlignmentToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlignmentToolApp.swift
+//  AlignmentTool
+//
+//  Created by 존진 on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct AlignmentToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
