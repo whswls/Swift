538e07b1e06a8f3a624b3a3724b71568b375c7e6
EunJinCHO <till5941@gmail.com>
2025-01-14 13:30:34 +0900

    NavigationStack
    NavigationStack 실습

--- SwiftUI/ListDemo/ListDemo/ContentView.swift
+++ SwiftUI/ListDemo/ListDemo/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct TodoItem: Identifiable {
+struct TodoItem: Identifiable, Hashable {
     let id: UUID = UUID()
     let task: String
     var imageName: String
@@ -25,29 +25,49 @@ struct ContentView: View {
                  imageName: "car.fill"),
     ]
     var body: some View {
-        List {
-            Section(header: Text("Settings")) {
-                Toggle(isOn: $toggleStatus) {
-                    Text("Allow Notifications")
+        NavigationStack {
+            List {
+                Section(header: Text("Settings")) {
+                    Toggle(isOn: $toggleStatus) {
+                        Text("Allow Notifications")
+                    }
+                    NavigationLink(value: listData.count) {
+                        Text("View Task Count")
+                    }
                 }
-            }
-            Section(header: Text("To Do Tasks")) {
-                ForEach(listData) {
-                    item in
-                    HStack {
-                        Image(systemName: item.imageName)
-                        Text(item.task)
+                
+                Section(header: Text("To Do Tasks")) {
+                    ForEach(listData) { item in
+                        NavigationLink(value: item) {
+                            HStack {
+                                Image(systemName: item.imageName)
+                                Text(item.task)
+                            }
+                        }
                     }
                 }
             }
+            // NavigationLink value 타입에 따른 뷰 빌더를 실행시켜주는 수정자
+            .navigationDestination(for: TodoItem.self) { item in
+                Text("Number of tasks: \(item.task) (\(item.id)")
+            }
+            .navigationDestination(for: Int.self) { count in
+                Text("Number of tasks: \(count)")
+            }
+            .navigationDestination(for: String.self) { task in
+                VStack {
+                    Text("Selected Task: \(task)")
+                }
+            }
+            .refreshable {
+                listData = [
+                    TodoItem(task: "Order dinner", imageName: "dollarsign.circle.fill"),
+                    TodoItem(task: "Call financial advisor", imageName: "phone.fill"),
+                    TodoItem(task: "Sell the kids", imageName: "person.2.fill")
+                ]
+            }
         }
-        .refreshable {
-            listData = [
-                TodoItem(task: "Order dinner", imageName: "dollarsign.circle.fill"),
-                TodoItem(task: "Call financial advisor", imageName: "phone.fill"),
-                TodoItem(task: "Sell the kids", imageName: "person.2.fill")
-            ]
-        }
+        
     }
 }
 
