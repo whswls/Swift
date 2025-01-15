b8bc9bf1cd14b4432046689ec0fab30298fd102c
EunJinCHO <till5941@gmail.com>
2025-01-15 14:13:19 +0900

    트랜지션
    트랜지션 실습

--- SwiftUI/AnimationDemoApp/AnimationDemoApp/ContentView.swift
+++ SwiftUI/AnimationDemoApp/AnimationDemoApp/ContentView.swift
@@ -8,38 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var offset: CGSize = .zero
-    @State private var scale: CGFloat = 1.0
+    @State private var isTextVisible: Bool = false
     
     var body: some View {
-        Circle()
-            .fill(Color.green)
-            .frame(width: 100, height: 100)
-            .scaleEffect(scale)
-            .animation(
-                .timingCurve(0.68, -0.6, 0.32, 1.6)
-                .delay(0.2)
-                .repeatCount(3, autoreverses: true),
-                value: scale
-            )
-            .onTapGesture {
-                scale = scale < 1.5 ? 2.0 : 1.0
+        VStack(spacing: 20) {
+            Button(action: {
+                withAnimation(.easeIn(duration: 0.8)) {
+                    isTextVisible.toggle()
+                }
+            }, label: {
+                Text("Toggle Text")
+                    .font(.title2)
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(Color.blue)
+                    .clipShape(.rect(cornerRadius: 10))
+            })
+            
+            if isTextVisible{
+                Text("Hello SwiftUI Animation!")
+                    .font(.largeTitle)
+                    .padding()
+                    .transition(.move(edge: .leading))
             }
-            .offset(offset)
-            .gesture(
-                DragGesture()
-                    .onChanged { gesture in
-                        withAnimation {
-                            self.offset =
-                                gesture.translation
-                        }
-                    }
-                    .onEnded { _ in
-                        withAnimation(.spring()) {
-                            self.offset = .zero
-                        }
-                    }
-            )
+        }
     }
 }
 
