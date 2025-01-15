5c77c3c8b60a448cf3c8d17b1e727f770dd51846
EunJinCHO <till5941@gmail.com>
2025-01-15 13:18:39 +0900

    TabView 의 selection 상태를 이용한 버튼 이벤트 예시
    TabView 의 selection 상태를 이용한 버튼 이벤트 예시 실습

--- SwiftUI/TabViewDemo/TabViewDemo/ContentView.swift
+++ SwiftUI/TabViewDemo/TabViewDemo/ContentView.swift
@@ -32,7 +32,27 @@ struct ContentView: View {
                 .tag(2)
         }
         .font(.largeTitle)
-        //.tabViewStyle(PageTabViewStyle())
+        // 페이지 뷰 형태의 탭을 구성하는 예시
+        .tabViewStyle(PageTabViewStyle())
+        // 별도의 버튼을 이용해 탭을 이동
+        HStack {
+            Spacer()
+            Button("1") {
+                selection = 0
+            }
+            .disabled(selection == 0)
+            Spacer()
+            Button("2") {
+                selection = 1
+            }
+            .disabled(selection == 1)
+            Spacer()
+            Button("3") {
+                selection = 2
+            }
+            .disabled(selection == 2)
+            Spacer()
+        }
     }
 }
 
