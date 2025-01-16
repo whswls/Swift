715a0037b18683525c87559636401e33d2159d27
EunJinCHO <till5941@gmail.com>
2025-01-16 14:00:56 +0900

    추가 삭제

--- SwiftUI/CoreDataDemo/CoreDataDemo/ContentView.swift
+++ SwiftUI/CoreDataDemo/CoreDataDemo/ContentView.swift
@@ -50,6 +50,7 @@ struct ContentView: View {
                             Text(product.quantity ?? "Not found")
                         }
                     }
+                    .onDelete(perform: deleteItems)
                 }
                 .navigationTitle(Text("Products"))
             }
@@ -59,11 +60,35 @@ struct ContentView: View {
     }
     
     private func addProduct() {
-        
+        withAnimation {
+            let newProduct = Product(context: viewContext)
+            newProduct.name = name
+            newProduct.quantity = quantity
+            saveContext()
+        }
     }
     
     private func deleteItems(offsets: IndexSet) {
-        
+        withAnimation {
+            offsets.map {
+                offset in
+                products[offset]
+            }.forEach({
+                element in
+                viewContext.delete(element)
+            })
+            // offsets.map{ products[$0] }.forEach(viewContext.delete)
+            saveContext()
+        }
+    }
+    
+    private func saveContext() {
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
     }
 }
 
