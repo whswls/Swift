aba06f0baaaae1a96c8499b0025849fa2065d94f
EunJinCHO <till5941@gmail.com>
2025-01-16 13:20:50 +0900

    사용자 인터페이스 설계

--- SwiftUI/CoreDataDemo/CoreDataDemo/ContentView.swift
+++ SwiftUI/CoreDataDemo/CoreDataDemo/ContentView.swift
@@ -9,77 +9,63 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    @State var name: String = ""
+    @State var quantity: String = ""
+    
     @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
+    @FetchRequest(entity: Product.entity(), sortDescriptors: [])
+    private var products: FetchedResults<Product>
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        NavigationStack {
+            VStack {
+                TextField("Product name", text: $name)
+                TextField("Product quantity", text: $quantity)
+                
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        addProduct()
+                    }, label: {
+                        Text("Add")
+                    })
+                    Spacer()
+                    Button(action: {
+                        name = ""
+                        quantity = ""
+                    }, label: {
+                        Text("Clear")
+                    })
+                    Spacer()
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                
+                List {
+                    ForEach(products) { product in
+                        HStack {
+                            Text(product.name ?? "Not found")
+                            Spacer()
+                            Text(product.quantity ?? "Not found")
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
+                .navigationTitle(Text("Products"))
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+            .padding()
+            .textFieldStyle(RoundedBorderTextFieldStyle())
         }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+    
+    private func addProduct() {
+        
     }
-
+    
     private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
--- SwiftUI/CoreDataDemo/CoreDataDemo/Persistence.swift
+++ SwiftUI/CoreDataDemo/CoreDataDemo/Persistence.swift
@@ -14,9 +14,9 @@ struct PersistenceController {
     static let preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+        for i in 0..<10 {
+            let newItem = Product(context: viewContext)
+            newItem.name = "Product \(i)"
         }
         do {
             try viewContext.save()
