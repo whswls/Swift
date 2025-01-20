5226273c41b1275febd2a62d745c06471ab837f9
EunJinCHO <till5941@gmail.com>
2025-01-20 09:31:29 +0900

    SiriDemo
    SiriDemo 실습

---
+++ SwiftUI/SiriDemo/SiriDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SiriDemo
+//
+//  Created by 존진 on 1/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/SiriDemo/SiriDemo/SiriDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiriDemoApp.swift
+//  SiriDemo
+//
+//  Created by 존진 on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SiriDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
