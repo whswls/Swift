ea2f2fbf178fd7d04461749af0b036b151e98c47
EunJinCHO <till5941@gmail.com>
2025-01-20 10:33:24 +0900

    Intent Extension
    Intent Extension 실습

--- SwiftUI/SiriDemo/SiriDemo/SiriDemoApp.swift
+++ SwiftUI/SiriDemo/SiriDemo/SiriDemoApp.swift
@@ -6,12 +6,21 @@
 //
 
 import SwiftUI
+import Intents
 
 @main
 struct SiriDemoApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .onChange(of: scenePhase, initial: true) {
+            INPreferences.requestSiriAuthorization { status in
+            // 권한 상태 처리
+            print("Siri authorization status: \(status.rawValue)")
+            }
         }
     }
 }
