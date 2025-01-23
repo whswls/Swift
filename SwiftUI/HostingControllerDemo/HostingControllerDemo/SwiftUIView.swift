4f5dd092adac17d761fe7355b8fc06db579aff0a
EunJinCHO <till5941@gmail.com>
2025-01-23 15:34:37 +0900

    HostingController 추가

---
+++ SwiftUI/HostingControllerDemo/HostingControllerDemo/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  HostingControllerDemo
+//
+//  Created by 존진 on 1/23/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
