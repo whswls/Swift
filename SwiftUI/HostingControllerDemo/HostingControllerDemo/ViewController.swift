7690f83a5869c5a64a1018dee14ba1e339e211ed
EunJinCHO <till5941@gmail.com>
2025-01-23 16:09:14 +0900

    코드추가

--- SwiftUI/HostingControllerDemo/HostingControllerDemo/ViewController.swift
+++ SwiftUI/HostingControllerDemo/HostingControllerDemo/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let swiftUIViewController = UIHostingController(rootView: SwiftUIView())
+        addChild(swiftUIViewController)
+        swiftUIViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(swiftUIViewController.view)
+        
+        swiftUIViewController.view.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        swiftUIViewController.view.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        swiftUIViewController.didMove(toParent: self)
     }
 
-
+    
+    @IBSegueAction func showSwiftUIView(_ coder: NSCoder) -> UIViewController? {
+        return UIHostingController(coder: coder, rootView: SwiftUIView())
+    }
+    
+    @IBSegueAction func embedSwiftUIView(_ coder: NSCoder) -> UIViewController? {
+        return UIHostingController(coder: coder, rootView: SwiftUIView())
+    }
+    
 }
 
