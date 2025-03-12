ef8221034eef32961b29cb670e2e82c2e870e7c5
EunJinCHO <till5941@gmail.com>
2025-03-12 15:39:34 +0900

    SecondViewController에 메시지 입력 필드 및 제출 버튼 추가: delegate를 통해 메시지 전달 기능 구현

--- UIKit/HelloWorld/HelloWorld/SecondViewController.swift
+++ UIKit/HelloWorld/HelloWorld/SecondViewController.swift
@@ -13,52 +13,75 @@ protocol SecondViewControllerDelegate: AnyObject {
 
 class SecondViewController: UIViewController {
     weak var delegate: SecondViewControllerDelegate?
-
+    
+    lazy var messageTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "메시지를 입력하세요."
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("1 ViewController.viewDidLoad()")
-        
+        // Do any additional setup after loading the view.
+        print("01 SecondViewController.viewDidLoad()")
         setupUI()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("2 ViewController.viewWillAppear()")
-      }
-
-      override func viewIsAppearing(_ animated: Bool) {
+        print("02 SecondViewController.viewWillAppear()")
+    }
+    
+    override func viewIsAppearing(_ animated: Bool) {
         super.viewIsAppearing(animated)
-        print("3 ViewController.viewIsAppearing()")
-      }
-
-      override func viewDidAppear(_ animated: Bool) {
+        print("03 SecondViewController.viewIsAppearing()")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print("4 ViewController.viewDidAppear()")
-      }
-
-      override func viewWillDisappear(_ animated: Bool) {
+        print("04 SecondViewController.viewDidAppear()")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        print("5 ViewController.viewWillDisappear()")
-      }
-
-      override func viewDidDisappear(_ animated: Bool) {
+        print("05 SecondViewController.viewWillDisappear()")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        print("6 ViewController.viewDidDisappear()")
-          
-        // SecondViewController가 사라질 때 delegate에게 메시지 전달
-        delegate?.didDismissSecondViewController(message: "Bye!!")
-      }
+        print("06 SecondViewController.viewDidDisappear()")
+        
+    }
     
     func setupUI() {
+        print("07 SecondViewController.setupUI()")
+        // SecondViewController의 배경색을 노란색으로 설정
         self.view.backgroundColor = .yellow
         
         let label = UILabel()
         label.text = "Second View"
         label.textAlignment = .center
         label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 22)
+        label.font = UIFont.systemFont(ofSize: 24)
         label.frame = CGRect(x: 20, y: 100, width: view.frame.width - 40, height: 40)
         self.view.addSubview(label)
+        
+        messageTextField.frame = CGRect(x: 20, y: 200, width: view.frame.width - 40, height: 40)
+        self.view.addSubview(messageTextField)
+        
+        let button = UIButton()
+        button.setTitle("Submit", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.frame = CGRect(x: 20, y: 300, width: view.frame.width - 40, height: 40)
+        
+        button.addAction(UIAction { [weak self] _ in
+            self?.dismiss(animated: true)
+            // SecondViewController가 사라질 때 delegate에게 메시지를 전달
+            self?.delegate?.didDismissSecondViewController(message: self?.messageTextField.text ?? "")
+        }, for: .touchUpInside)
+        
+        self.view.addSubview(button)
     }
-
+    
 }
--- UIKit/HelloWorld/HelloWorld/ViewController.swift
+++ UIKit/HelloWorld/HelloWorld/ViewController.swift
@@ -8,37 +8,74 @@
 import UIKit
 
 class ViewController: UIViewController, SecondViewControllerDelegate {
-
+    
+    // lazy var: 초기화를 늦게 하기 위해 사용하는 키워드
+    lazy var myLabel: UILabel = {
+        print("myLabel 생성")
+        let label = UILabel()
+        label.text = "입력 결과를 출력합니다."
+        label.textAlignment = .center
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Do any additional setup after loading the view.
+        print("01 ViewController.viewDidLoad()")
         setupUI()
     }
     
     func setupUI() {
-        print("7 ViewController.setupUI()")
         let label = UILabel()
         label.text = "Hello, World!"
+        // (content layout) 라벨의 텍스트를 가운데 정렬
         label.textAlignment = .center
         label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 25)
-        // frame layout 붙여질 뷰의 위치와 크기 설정
-        label.frame = CGRect(x: 20, y: 100, width: view.frame.width - 40, height: 40)
-        // self.view(메인 뷰)에 라벨 추가
-        view.addSubview(label)
+        label.font = UIFont.systemFont(ofSize: 24)
+        // autolayout 설정
+        label.translatesAutoresizingMaskIntoConstraints = false
+        // self.view(메인 뷰)에 라벨을 서브 뷰로 추가
+        self.view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100),
+            label.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
+            label.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
+            label.heightAnchor.constraint(equalToConstant: 40)
+        ])
         
         // 버튼 추가
+        
         let button = UIButton()
-        button.setTitle("Go second", for: .normal)
+        button.setTitle("Go Second", for: .normal)
         button.setTitleColor(.blue, for: .normal)
-        button.frame = CGRect(x: 20, y: 200, width: view.frame.width - 40, height: 40)
         button.addTarget(self, action: #selector(goSecond), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
         
         self.view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: label.topAnchor, constant: 100),
+            button.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
+            button.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
+            button.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        self.view.addSubview(myLabel)
+        
+        NSLayoutConstraint.activate([
+            myLabel.topAnchor.constraint(equalTo: button.topAnchor, constant: 100),
+            myLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
+            myLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
+            myLabel.heightAnchor.constraint(equalToConstant: 40)
+        ])
     }
     
-    func didDismissSecondViewController(message: String){
-        print("SecondViewController에서 전달받은 메시지: \(message)")
+    func didDismissSecondViewController(message: String) {
+        myLabel.text = message
     }
     
     @objc func goSecond() {
@@ -46,6 +83,4 @@ class ViewController: UIViewController, SecondViewControllerDelegate {
         secondVC.delegate = self
         self.present(secondVC, animated: true)
     }
-
 }
-
