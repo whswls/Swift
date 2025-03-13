779380d3eea2f8a8038c5de0b2b4edff6770e728
EunJinCHO <till5941@gmail.com>
2025-03-13 09:12:11 +0900

    DelegateDemo 초기 프로젝트 생성

---
+++ UIKit/DelegateDemo/DelegateDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegateDemo
+//
+//  Created by 존진 on 3/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
