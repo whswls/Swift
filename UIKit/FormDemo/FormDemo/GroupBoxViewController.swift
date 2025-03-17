b7c09f9987430867512130cef405cc63bfb2e77d
EunJinCHO <till5941@gmail.com>
2025-03-17 10:13:07 +0900

    그룹 박스 뷰에 레이블, 토글, 텍스트 필드 추가 및 레이아웃 제약 조건 설정

--- UIKit/FormDemo/FormDemo/GroupBoxViewController.swift
+++ UIKit/FormDemo/FormDemo/GroupBoxViewController.swift
@@ -11,7 +11,7 @@ class GroupBoxViewController: UIViewController {
     
     var flag = false
     let groupBox = UIView()
-    let groupBodxLabel = UILabel()
+    let groupBoxLabel = UILabel()
     let toggle = UISwitch()
     let textField = UITextField()
 
@@ -30,14 +30,50 @@ class GroupBoxViewController: UIViewController {
         groupBox.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(groupBox)
         
+        groupBoxLabel.text = "Group Box"
+        groupBoxLabel.font = .systemFont(ofSize: 20)
+        groupBoxLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(groupBoxLabel)
+        
+        toggle.addTarget(self, action: #selector(toggleChanged), for: .valueChanged)
+        toggle.translatesAutoresizingMaskIntoConstraints = false
+        groupBox.addSubview(toggle)
+        
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Enter text..."
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        groupBox.addSubview(textField)
+        
         NSLayoutConstraint.activate([
             groupBox.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
             groupBox.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             groupBox.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            groupBox.heightAnchor.constraint(equalToConstant: 200)
+            groupBox.heightAnchor.constraint(equalToConstant: 200),
+            
+            groupBoxLabel.topAnchor.constraint(equalTo: groupBox.topAnchor, constant: 20),
+            groupBoxLabel.leadingAnchor.constraint(equalTo: groupBox.leadingAnchor, constant: 10),
+            
+            toggle.topAnchor.constraint(equalTo: groupBoxLabel.bottomAnchor, constant: 10),
+            toggle.leadingAnchor.constraint(equalTo: groupBox.leadingAnchor, constant: 10),
+            
+            textField.topAnchor.constraint(equalTo: toggle.bottomAnchor, constant: 10),
+            textField.leadingAnchor.constraint(equalTo: groupBox.leadingAnchor, constant: 10),
+            textField.trailingAnchor.constraint(equalTo: groupBox.trailingAnchor, constant: -10),
+            textField.bottomAnchor.constraint(equalTo: groupBox.bottomAnchor, constant: -10)
+            
+            
         ])
+    }
+    
+    @objc func toggleChanged() {
+        flag = toggle.isOn
+        print("flag: \(flag)")
     }
 
 
+}
+
+#Preview {
+    GroupBoxViewController()
 }
 
