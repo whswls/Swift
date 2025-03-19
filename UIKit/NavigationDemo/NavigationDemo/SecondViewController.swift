43d1ba119be34a9bf94bc75c3c67b1271e4e9634
EunJinCHO <till5941@gmail.com>
2025-03-19 10:21:51 +0900

    네비게이션 바에 여러 Bar 버튼 추가

--- UIKit/NavigationDemo/NavigationDemo/SecondViewController.swift
+++ UIKit/NavigationDemo/NavigationDemo/SecondViewController.swift
@@ -11,6 +11,7 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "두번째 뷰"
 
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
@@ -23,6 +24,52 @@ class SecondViewController: UIViewController {
         navigationController?.navigationBar.standardAppearance = appearance
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
         navigationController?.navigationBar.tintColor = .white
+        
+        let leftButton = UIBarButtonItem(
+            systemItem: .cancel,
+            primaryAction: UIAction{ [weak self] _ in
+            self?.leftButtonTapped()
+        })
+        navigationItem.leftBarButtonItem = leftButton
+        
+        let rightButton = UIBarButtonItem(
+            title: "완료",
+            primaryAction: UIAction{ [weak self] _ in
+            self?.rightButtonTapped()
+        })
+        
+        let editButton = UIBarButtonItem(
+            systemItem: .edit,
+            primaryAction: UIAction{ [weak self] _ in
+            self?.editButtonTapped()
+        })
+        
+        let searchButton = UIBarButtonItem(
+            systemItem: .search,
+            primaryAction: UIAction{ [weak self] _ in
+            self?.searchButtonTapped()
+        })
+        
+        navigationItem.rightBarButtonItems = [rightButton, editButton, searchButton]
+        
+    }
+    
+    func leftButtonTapped() {
+        print("왼쪽 버튼 클릭")
+        // 네비게이션 백 버튼을 누른 것과 동일한 효과
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    func rightButtonTapped() {
+        print("오른쪽 버튼 클릭")
+    }
+    
+    func editButtonTapped() {
+        print("EDIT 버튼 클릭")
+    }
+    
+    func searchButtonTapped() {
+        print("SEARCH 버튼 클릭")
     }
 
 
