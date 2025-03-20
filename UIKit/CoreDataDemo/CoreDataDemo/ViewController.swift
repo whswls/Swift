74c2e6557b3108ff68bae1d02daaff3629576ad9
EunJinCHO <till5941@gmail.com>
2025-03-20 11:37:40 +0900

    Core Data 모델, 뷰 컨트롤러 및 스토리보드 추가

---
+++ UIKit/CoreDataDemo/CoreDataDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by 존진 on 3/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
