890345feb815fbbc2a633fb03fe7da5ff60d12db
EunJinCHO <till5941@gmail.com>
2025-03-25 14:01:20 +0900

    Journal 실습

---
+++ UIKit/JRNL/JRNL/JournalListViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JRNL
+//
+//  Created by 존진 on 3/25/25.
+//
+
+import UIKit
+
+class JournalListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension JournalListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath)
+    }
+}
