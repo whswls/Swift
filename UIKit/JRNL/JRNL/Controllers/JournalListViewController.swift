c9ab864bc970b78f15055abeffdf0badd3ec2eeb
EunJinCHO <till5941@gmail.com>
2025-03-26 14:32:42 +0900

    코드정렬

--- UIKit/JRNL/JRNL/Controllers/JournalListViewController.swift
+++ UIKit/JRNL/JRNL/Controllers/JournalListViewController.swift
@@ -8,67 +8,67 @@
 import UIKit
 
 class JournalListViewController: UIViewController {
-  
-  @IBOutlet weak var tableView: UITableView!
-  
-  var sampleJournalEntryData = SampleJournalEntryData()
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // 샘플 데이터 생성
-    sampleJournalEntryData.createSampleJournalEntryData()
-  }
-  
-  @IBAction func unwindNewEntryCancel(segue: UIStoryboardSegue) {
-    print("unwindNewEntryCancel")
-  }
-  
-  @IBAction func unwindNewEntrySave(segue: UIStoryboardSegue) {
-    if let sourceViewController = segue.source as? AddJournalEntryViewController,
-       let newJournalEntry = sourceViewController.newJournalEntry {
-      sampleJournalEntryData.journalEntries.append(newJournalEntry)
-      tableView.reloadData()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var sampleJournalEntryData = SampleJournalEntryData()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 샘플 데이터 생성
+        sampleJournalEntryData.createSampleJournalEntryData()
     }
     
-  }
+    @IBAction func unwindNewEntryCancel(segue: UIStoryboardSegue) {
+        print("unwindNewEntryCancel")
+    }
+    
+    @IBAction func unwindNewEntrySave(segue: UIStoryboardSegue) {
+        if let sourceViewController = segue.source as? AddJournalEntryViewController,
+           let newJournalEntry = sourceViewController.newJournalEntry {
+            sampleJournalEntryData.journalEntries.append(newJournalEntry)
+            tableView.reloadData()
+        }
+        
+    }
 }
 
 extension JournalListViewController: UITableViewDataSource {
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return sampleJournalEntryData.journalEntries.count
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let journalCell = tableView.dequeueReusableCell(
-      withIdentifier: "journalCell",
-      for: indexPath
-    ) as! JournalListTableViewCell
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sampleJournalEntryData.journalEntries.count
+    }
     
-    let journalEntry = sampleJournalEntryData.journalEntries[indexPath.row]
-    // 날짜, 제목, 사진 표시
-    // 날짜는 "월 일, 년" 형식으로 표시
-    journalCell.dateLabel.text = journalEntry.date.formatted(.dateTime.month().day().year())
-    journalCell.titleLabel.text = journalEntry.entryTitle
-    journalCell.photoImageView.image = journalEntry.photo
-    
-    return journalCell
-  }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let journalCell = tableView.dequeueReusableCell(
+            withIdentifier: "journalCell",
+            for: indexPath
+        ) as! JournalListTableViewCell
+        
+        let journalEntry = sampleJournalEntryData.journalEntries[indexPath.row]
+        // 날짜, 제목, 사진 표시
+        // 날짜는 "월 일, 년" 형식으로 표시
+        journalCell.dateLabel.text = journalEntry.date.formatted(.dateTime.month().day().year())
+        journalCell.titleLabel.text = journalEntry.entryTitle
+        journalCell.photoImageView.image = journalEntry.photo
+        
+        return journalCell
+    }
 }
 
 
 extension JournalListViewController: UITableViewDelegate {
-  func tableView(
-    _ tableView: UITableView,
-    commit editingStyle: UITableViewCell.EditingStyle,
-    forRowAt indexPath: IndexPath
-  ) {
-    if editingStyle == .delete {
-      sampleJournalEntryData.journalEntries.remove(at: indexPath.row)
-      // 테이블 전체 새로고침
-      // tableView.reloadData()
-      
-      // 테이블에서 해당 행만 삭제 ( 애니메이션 효과 포함 )
-      tableView.deleteRows(at: [indexPath], with: .fade)
+    func tableView(
+        _ tableView: UITableView,
+        commit editingStyle: UITableViewCell.EditingStyle,
+        forRowAt indexPath: IndexPath
+    ) {
+        if editingStyle == .delete {
+            sampleJournalEntryData.journalEntries.remove(at: indexPath.row)
+            // 테이블 전체 새로고침
+            // tableView.reloadData()
+            
+            // 테이블에서 해당 행만 삭제 ( 애니메이션 효과 포함 )
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
-  }
 }
