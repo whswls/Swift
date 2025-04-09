af1e3ae7ed3d5e10cc14d42613bcbed1efbffae9
EunJinCHO <till5941@gmail.com>
2025-04-09 09:51:32 +0900

    CombineFirebaseBook 추가

---
+++ Combine/CombineFirebaseBook/CombineFirebaseBook/CombineFirebaseBookApp.swift
@@ -0,0 +1,26 @@
+//
+//  CombineFirebaseBookApp.swift
+//  CombineFirebaseBook
+//
+//  Created by 존진 on 4/9/25.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate:NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+@main
+struct CombineFirebaseBookApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
