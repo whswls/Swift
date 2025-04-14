088fca86f4a99d0bf63ea6a154f2b2a134161049
EunJinCHO <till5941@gmail.com>
2025-04-14 13:43:48 +0900

    에뮬레이터 코드 추가

--- Combine/CombineFirebaseBook/CombineFirebaseBook/CombineFirebaseBookApp.swift
+++ Combine/CombineFirebaseBook/CombineFirebaseBook/CombineFirebaseBookApp.swift
@@ -7,10 +7,16 @@
 
 import SwiftUI
 import FirebaseCore
+import FirebaseAuth
+import FirebaseFirestore
 
 class AppDelegate:NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        // 에뮬레이터 설정
+        Auth.auth().useEmulator(withHost: "localhost", port: 9099)
+        
+        Firestore.firestore().useEmulator(withHost: "localhost", port: 8080)
         return true
     }
 }
