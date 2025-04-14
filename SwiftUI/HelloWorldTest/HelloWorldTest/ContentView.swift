cb3b170811a5bb8ede833719700ac39a32b9cb6c
EunJinCHO <till5941@gmail.com>
2025-04-14 14:23:56 +0900

    XCTest 코드 작성(UITest 코드 삭제)

---
+++ SwiftUI/HelloWorldTest/HelloWorldTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloWorldTest
+//
+//  Created by 존진 on 4/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/HelloWorldTest/HelloWorldTest/HelloWorldTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldTestApp.swift
+//  HelloWorldTest
+//
+//  Created by 존진 on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
